f64af29e88939bc1c235f2789e588a0a8ed93db5
Kozar Daniil <146545649+Velogresss@users.noreply.github.com>
2024-11-23 17:30:00 +0200

    Fix Bug

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import RegistrationForm from '../components/UI/RegistrationForm/RegistrationForm'; 
 import classes from './register.module.css'
+import GetToken from '../components/UI/GetToken/GetToken' 
 
 function register() {
     return (
         <div className={classes.registerConatainer}>
+            <GetToken/>
             <h1>Реєстрація</h1>
             <RegistrationForm/>
         </div>
